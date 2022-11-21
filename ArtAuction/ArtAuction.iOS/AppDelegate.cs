afd859cf8363717b1d1288d4b0e59684383dfb6b
estivenson1 <csharp_eov@hotmail.com>
2022-11-20 23:49:49 -0500

    Finalización migración
    El proyecto de iOS no se ejecutaba por problemas en el archivo .sln,
    se eliminaron 2 líneas  y se actualizaron 2 líneas

--- ArtAuction/ArtAuction.iOS/AppDelegate.cs
+++ ArtAuction/ArtAuction.iOS/AppDelegate.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 
 using Foundation;
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
 using UIKit;
 
 namespace ArtAuction.iOS
@@ -11,21 +13,23 @@ namespace ArtAuction.iOS
     // User Interface of the application, as well as listening (and optionally responding) to 
     // application events from iOS.
     [Register("AppDelegate")]
-    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    public partial class AppDelegate : MauiUIApplicationDelegate //global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
-        //
-        // This method is invoked when the application has loaded and is ready to run. In this 
-        // method you should instantiate the window, load the UI into it and then make the window
-        // visible.
-        //
-        // You have 17 seconds to return from this method, or iOS will terminate your application.
-        //
-        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
-        {
-            global::Xamarin.Forms.Forms.Init();
-            LoadApplication(new App());
+        ////
+        //// This method is invoked when the application has loaded and is ready to run. In this 
+        //// method you should instantiate the window, load the UI into it and then make the window
+        //// visible.
+        ////
+        //// You have 17 seconds to return from this method, or iOS will terminate your application.
+        ////
+        //public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        //{
+        //    global::Xamarin.Forms.Forms.Init();
+        //    LoadApplication(new App());
 
-            return base.FinishedLaunching(app, options);
-        }
+        //    return base.FinishedLaunching(app, options);
+        //}
+
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
     }
 }
